7cf65019a101fd8f524f8fb3a81ccd9b9ace7bca
kuna7 <kuna728@gmail.com>
2021-02-02 01:05:33 +0100

    v0.1.5

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3.8
+
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
@@ -17,6 +19,7 @@ for line in plik:
 plik.close()
 
 zleodp = {}
+udzodp = {}
 
 sm = ScreenManager()
 
@@ -42,9 +45,16 @@ class MainScreen(Screen):
 
 	def koncz_runde(self, dt):
 		self.ids.mainlabel.color = 1, 1, 1, 1
+		self.ids.mainlabel.bold = False
 		del slownik[MainScreen.klucz]
 		if MainScreen.licznik == int(self.ids.sliderslowek.value):
 			self.ids.text_input.readonly=True
+			self.ids.text_input.text = 'ustaw i zacznij wpisywać'
+			self.ids.opcje.visible = True
+			self.ids.mainlabel.text = ''
+			self.ids.status.value = 0
+			self.ids.text_input.readonly = False
+			sm.get_screen('score').podsumowanie()
 			sm.current = 'score'
 		else:
 			index = random.randint(0, len(slownik)-1)
@@ -56,9 +66,11 @@ class MainScreen(Screen):
 		self.ids.text_input.text = ''
 		klucz = MainScreen.klucz
 		MainScreen.licznik+=1
+		self.ids.mainlabel.bold = True
 		if odp != klucz:
 			MainScreen.licznik_zlych+=1
 			zleodp[klucz] = slownik[klucz]
+			udzodp[klucz] = odp
 			self.ids.mainlabel.color = 1, 0, 0, 1
 		else:
 			self.ids.mainlabel.color = 60/255, 179/255, 113/255, 1
@@ -67,9 +79,10 @@ class MainScreen(Screen):
 class ScoreScreen(Screen):
 	def podsumowanie(self):
 		dobrych = MainScreen.licznik - MainScreen.licznik_zlych
-		podsumowanie = '''KONIEC\nPoprawnych: {0}\nBlednych: {1}\nProcentowo: {2}%\n\nZłe odpowiedzi:\n'''.format(str(dobrych), str(MainScreen.licznik_zlych), str(100*dobrych/MainScreen.licznik))
+		podsumowanie = '\n\n[b][size=24]KONIEC[/size][/b]\n\n[color=50f700]Poprawnych:[/color] {0}\n[color=ff3333]Blednych: [/color]{1}\n[color=d2e3ff]Procentowo: [/color]{2}%\n\n'.format(str(dobrych),
+			str(MainScreen.licznik_zlych), str(100*dobrych/MainScreen.licznik)) + '\n\n[size=20]Złe odpowiedzi:[/size]\n\n'
 		for i in zleodp:
-			podsumowanie+= '{0} - {1}\n'.format(i,zleodp[i])
+			podsumowanie+= 50 * '{0} - [color=50f700]{1}[/color] / [color=ff3333]{2}[/color]\n\n'.format(zleodp[i],i,udzodp[i])
 		self.ids.podsumowanie.text = podsumowanie
 
 class WordsApp(App):
